d06f5fe5b8998225ff813cf73c1ddbf1389232c6
DK_O <odk1193@gmail.com>
2019-07-09 13:27:41 +0900

    전투구현중단, 사운드 넣는중.

--- HearthStoneVR/Assets/03.Scripts/GameManager.cs
+++ HearthStoneVR/Assets/03.Scripts/GameManager.cs
@@ -7,10 +7,14 @@ public class GameManager : Photon.PunBehaviour
 {
     public Image img;
     public GameObject canvas;
-    bool startGame;
+    public AudioClip[] clips;
+
+    private AudioSource audioSource;
+    private bool startGame;
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -34,7 +38,27 @@ public class GameManager : Photon.PunBehaviour
         }
         canvas.SetActive(false);
         startGame = true;
+        audioSource.clip = clips[0];
+        audioSource.Play();
+    }
 
+    public void ChngeMusic(int musicNum, bool returnToOrigin){
+        float originalTime = audioSource.time;
+        float length = clips[musicNum].length;
 
+        audioSource.Stop();
+        audioSource.clip = clips[musicNum];
+        audioSource.Play();
+        if(returnToOrigin){
+            StartCoroutine(returnToBGM(length, originalTime));
+        }
+    }
+
+    IEnumerator returnToBGM(float length, float originalTime){
+        yield return new WaitForSeconds(length);
+        audioSource.Stop();
+        audioSource.clip = clips[0];
+        audioSource.time = originalTime;
+        audioSource.Play();
     }
 }
