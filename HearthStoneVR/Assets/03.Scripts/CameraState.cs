e69ab5528c637bef802262c9273f483228c25d52
smpk32 <smpk32@naver.com>
2019-07-09 13:00:23 +0900

    Revert "플레이어컨트롤 수정"
    This reverts commit 0d393e03e8f4ad2711ecbb7429e4f429fcebabc8.

--- HearthStoneVR/Assets/03.Scripts/CameraState.cs
+++ HearthStoneVR/Assets/03.Scripts/CameraState.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraState : Photon.PunBehaviour
 {
+
+    public 
     // Start is called before the first frame update
     void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         if (!photonView.isMine)
         {
             gameObject.SetActive(false);
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
