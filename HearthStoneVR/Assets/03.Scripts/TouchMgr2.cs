654cd361556ba24d34366b525eb0f9356f4bece4
DK_O <odk1193@gmail.com>
2019-07-10 20:42:05 +0900

    VR모드 구현

---
+++ HearthStoneVR/Assets/03.Scripts/TouchMgr2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchMgr2 : MonoBehaviour
+{
+    private Transform tr;
+    private LineRenderer line;
+
+    private Ray ray;
+    private RaycastHit hit;
+    private Camera cam;
+    private int layerBT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        line = GetComponent<LineRenderer>();
+        layerBT = 1 << LayerMask.NameToLayer("BUTTON");
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = new Ray(tr.position, tr.forward);
+        if (Physics.Raycast(ray, out hit, 16.0f))
+        {
+            float dist = hit.distance;
+            line.SetPosition(1, new Vector3(0, 0, dist));
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            // Debug.DrawRay(ray.origin, ray.direction * 100.0f, Color.green);
+            if (Physics.Raycast(ray, out hit, 100.0f, layerBT))
+            {
+                hit.collider.GetComponent<Button>().onClick.Invoke();
+            }
+        }
+    }
+}
