a3586d918c5d0072ae5bf78aacb8217b52ba4155
LEE BYUNGNAM <bol0615@naver.com>
2019-07-10 17:31:08 +0900

    플레이어 위치 수정

--- HearthStoneVR/Assets/03.Scripts/CardPosition.cs
+++ HearthStoneVR/Assets/03.Scripts/CardPosition.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class CardPosition : Photon.MonoBehaviour, IPunObservable
 {
     private Transform handCards;
+    private GameTouchMgr gtm;
+    public GameObject sommonedCard;
+
+    private void Start()
+    {
+        gtm = GameObject.Find("GameTouchMgr1").GetComponent<GameTouchMgr>();
+    }
+
+    [PunRPC]
+    public void CardInstantiate(Vector3 pos, Quaternion rot, string tag)
+    {
+        sommonedCard = Instantiate(Resources.Load("FIELD_" + tag) as GameObject, pos, rot, gtm.hitObject);
+    }
+
     [PunRPC]
     void CardMove(Vector3 pos, Quaternion rot, bool whoDeck)
     {
